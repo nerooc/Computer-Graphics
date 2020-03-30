02c4e845c7dfe45de650a15c4dade7def2f65120
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-03-30 17:27:13 +0200

    added Lab4

---
+++ Lab4 - WxWidgets2D/ChartClass.cpp
@@ -0,0 +1,326 @@
+#include <wx/DC.h>
+#include <memory>
+
+#include "ChartClass.h"
+#include "vecmat.h"
+
+#define PI 3.14159265359
+
+//FUNKCJE NA MACIERZACH////////////////////////////////
+
+//FUNKCJA TRANSLACJI - POZWALA NA PORUSZANIE WYKRESEM
+//MACIERZ TRANSLACJI:
+//	 _  	_	
+//	| 1 0 Tx |
+//	| 0 1 Ty |
+//	| 0 0 1  |
+//	|_      _|
+
+Matrix ChartClass::Set_Translate(double Tx, double Ty)
+{
+	Matrix translateM;
+
+	translateM.data[0][0] = 1;
+	translateM.data[1][0] = 0;
+	translateM.data[1][1] = 1;
+	translateM.data[0][1] = 0;
+	translateM.data[2][2] = 1;
+	translateM.data[0][2] = Tx;
+	translateM.data[1][2] = Ty;
+
+	return translateM;
+}
+
+//FUNKCJA SKALUJ¥CA
+//MACIERZ SKALUJ¥CA:
+//
+//	 _   _				
+//	|Sx  0|
+//	|0	Sy|
+//	|_   _|
+
+Matrix ChartClass::Set_Scale(int w, int h)
+{
+	Matrix scaleM;
+
+	double Sx = (w - 20.0) / (cfg->Get_x1() - cfg->Get_x0());
+	double Sy = (h - 20.0) / (cfg->Get_y1() - cfg->Get_y0());
+
+	scaleM.data[0][0] = Sx;
+	scaleM.data[1][0] = 0;
+	scaleM.data[1][1] = Sy;
+	scaleM.data[0][1] = 0;
+
+	return scaleM;
+}
+
+//FUNKCJA OBRACAJ¥CA
+//MACIERZ ROTACJI:
+//	 ___				___	
+//	|cos(alpha) -sin(alpha)|
+//	|sin(alpha)	cos(alpha) |
+//	 ___			    ___
+
+Matrix ChartClass::Set_Rotation(double alpha)
+{
+	Matrix rotationM;
+
+	double angle = alpha * PI / 180;
+
+	rotationM.data[0][0] = cos(angle);
+	rotationM.data[0][1] = sin(angle);
+	rotationM.data[1][0] = -sin(angle);
+	rotationM.data[1][1] = cos(angle);
+
+	return rotationM;
+}
+
+
+//FUNKCJE PODSTAWOWE//////////////////////////////////
+
+
+ChartClass::ChartClass(std::shared_ptr<ConfigClass> c)
+{
+	cfg = std::move(c);
+	x_step = 200;
+}
+
+void ChartClass::Set_Range()
+{
+	double xmin = 9999.9, xmax = -9999.9, ymin = 9999.9, ymax = -9999.9;
+	double x, y, step;
+	int i;
+
+	xmin = cfg->Get_x_start();
+	xmax = cfg->Get_x_stop();
+
+	step = (cfg->Get_x_stop() - cfg->Get_x_start()) / (double)x_step;
+	x = cfg->Get_x_start();
+
+	for (i = 0; i <= x_step; i++)
+	{
+		y = GetFunctionValue(x);
+		if (y > ymax) ymax = y;
+		if (y < ymin) ymin = y;
+		x = x + step;
+	}
+
+	y_min = ymin;
+	y_max = ymax;
+	x_min = xmin;
+	x_max = xmax;
+}
+
+
+double ChartClass::GetFunctionValue(double x)
+{
+	if (cfg->Get_F_type() == 1) return x * x;
+	if (cfg->Get_F_type() == 2) return 0.5 * exp(4 * x - 3 * x * x);
+	return x + sin(x * 4.0);
+}
+
+
+double ChartClass::Get_Y_min()
+{
+	Set_Range();
+	return y_min;
+}
+
+double ChartClass::Get_Y_max()
+{
+	Set_Range();
+	return y_max;
+}
+
+
+//FUNKCJE RYSUJ¥CE/OBS£UGUJ¥CE////////////////////////////////////
+
+
+void ChartClass::Draw(wxDC* DC, int w, int h)
+{
+	//USTALAMY CENTRUM
+	double x0, y0;
+	setCenter(x0, y0, w, h);
+
+	//ZMIENIAMY KOLOR T£A
+	DC->SetBackground(wxBrush(wxColour(*wxWHITE)));
+	
+	//CZYŒCIMY T£O
+	DC->Clear();
+
+	//ZMIENIAMY KOLOR NA CZERWONY
+	DC->SetPen(wxPen(wxColour(*wxRED)));
+
+	//RYSUJEMY PROSTOK¥T OKALAJ¥CY WYKRES
+	DC->DrawRectangle(10, 10, w - 20, h - 20);
+
+	//TWORZYMY MACIERZ TRANSFORMACJI
+	Matrix transformationM;
+	setTransformM(transformationM, x0, y0, w, h);
+	
+	//USTALAMY KROK
+	double step;
+	setStep(step);
+
+	//ustawiamy ClippingRegion zeby nie wychodzilo poza granice
+	DC->SetClippingRegion(10, 10, w - 20, h - 20);
+
+	//RYSUJEMY OSIE
+	drawAxes(DC, transformationM, w, h);
+	
+	//DEFINIUJÊ WARTOŒCI KTÓRYCH U¯YJÊ DO NARYSOWANIA OZNACZEÑ WARTOŒCI NA OSIACH
+	double v1 = cfg->Get_x_start() / 2;
+	double v2 = cfg->Get_x_stop() / 3;
+	double v3 = 2 * cfg->Get_x_stop() / 3;
+	double v4 = Get_Y_min() / 2;
+	double v5 = Get_Y_max() / 3;
+	double v6 = 2 * Get_Y_max() / 3;
+	
+	//U£ADNIENIE STRZA£EK I DANIE W FUNKCJÊ
+	drawArrows(DC, transformationM, w, h);
+	
+	//RYSUJEMY WARTOŒCI NA OSI X
+	drawMarkX(DC, transformationM, v1, w, h);
+	drawMarkX(DC, transformationM, v2, w, h);
+	drawMarkX(DC, transformationM, v3, w, h);
+	
+	//RYSUJEMY WARTOŒCI NA OSI Y
+	drawMarkY(DC, transformationM, v4, w, h);
+	drawMarkY(DC, transformationM, v5, w, h);
+	drawMarkY(DC, transformationM, v6, w, h);
+		
+	//RYSOWANIE FUNKCJI
+	drawFunction(DC, transformationM, w, h, step);
+	
+}
+
+//FUNKCJA ZA POMOC¥ KTÓREJ RYSUJEMY WSZYSTKO NA EKRANIE, WSZYSTKO CO MA BYÆ OBJÊTE TRANSFORMACJAMI
+void ChartClass::line2d(wxDC* DC,  Matrix transfromationM, double x1, double y1, double x2, double y2,  int w, int h)
+{
+	//PUNKT (WEKTOR) POCZ¥TKOWY
+	Vector init;
+	init.Set(x1, y1);
+	init = transfromationM * init;
+
+	//PUNKT (WEKTOR) KOÑCOWY
+	Vector extr;
+	extr.Set(x2, y2);
+	extr = transfromationM * extr;
+
+	//RYSUJEMY
+	DC->DrawLine(init.GetX(), init.GetY(), extr.GetX(), extr.GetY());
+
+}
+
+//SK£ADAMY MACIERZ TRANSFORMACJI
+void ChartClass::setTransformM(Matrix& transformationM, double x0, double y0, int w, int h) {
+	//ZA POMOC¥ INSTRUKCJI SWITCH ZALE¯NIE OD TEGO CZY MAMY OBRACAÆ DOOK£A ŒRODKA EKRANU LUB ŒRODKA UK£ADU, KOLEJNOŒÆ MNO¯EÑ JEST ZAMIENIONA
+	transformationM = (cfg->RotateScreenCenter() ? Set_Translate(x0, y0) * Set_Rotation(cfg->Get_Alpha()) * Set_Translate(cfg->Get_dX(), cfg->Get_dY()) * Set_Scale(w, h) : Set_Translate(x0, y0) * Set_Translate(cfg->Get_dX(), cfg->Get_dY()) * Set_Rotation(cfg->Get_Alpha()) * Set_Scale(w, h));
+}
+
+//FUNKCJA USTALAJ¥CA ŒRODEK
+void ChartClass::setCenter(double& x0, double& y0, int w, int h) {
+
+	//POBIERAMY WARTOŒCI ZA POMOC¥ FUNKCJI GET_[ELEMENT]
+	double actx0 = cfg->Get_x0();
+	double actx1 = cfg->Get_x1();
+	double acty0 = cfg->Get_y0();
+	double acty1 = cfg->Get_y1();
+
+	//DEKLARUJEMY X0 I Y0
+	x0 = - (((double)w - 20) / (actx1 - actx0)) * actx0 + 10;
+	y0 = - (((double)h - 20) / (acty0 - acty1)) * acty1 + 10;
+}
+
+//FUNKCJA RYSUJ¥CA OSIE X I Y
+void ChartClass::drawAxes(wxDC* DC, Matrix transformationM, int w, int h) {
+	
+	//POBIERAMY WARTOŒCI ZA POMOC¥ FUNKCJI GET_[ELEMENT]
+	double initX = cfg->Get_x_start();
+	double extrX = cfg->Get_x_stop();
+
+	double initY = -Get_Y_min();
+	double extrY = -Get_Y_max();
+
+	//ZMIENIAMY KOLOR NA NIEBIESKI
+	DC->SetPen(wxPen(wxColour(*wxBLUE)));
+
+	//RYSUJEMY OŒ X
+	line2d(DC, transformationM, initX, 0, extrX, 0, w, h);
+
+	//RYSUJEMY OŒ Y
+	line2d(DC, transformationM, 0, initY, 0, extrY, w, h);
+}
+
+//FUNKCJA RYSUJ¥CA STRZA£KI NA KOÑCACH OSI
+void ChartClass::drawArrows(wxDC* DC, Matrix transformationM, int w, int h) {
+
+	//POBIERAMY WARTOŒCI ZA POMOC¥ FUNKCJI GET_[ELEMENT]
+	double x1 = cfg->Get_x_stop();
+	double y1 = Get_Y_max();
+
+	//STRZA£KA NA OSI X
+	line2d(DC, transformationM, x1, 0, x1 - 0.08, 0.03, w, h);
+	line2d(DC, transformationM, x1, 0, x1 - 0.08, -0.03, w, h);
+	
+	//STRZA£KA NA OSI Y
+	line2d(DC, transformationM, 0, -y1, 0.03, -y1 + 0.08, w, h);
+	line2d(DC, transformationM, 0, -y1, -0.03, -y1 + 0.08, w, h);
+}
+
+//FUNKCJA RYSUJ¥CA WARTOŒCI NA OSI X
+void ChartClass::drawMarkX(wxDC* DC, Matrix transformationM, double v, int w, int h) {
+	
+	//PUNKT (WEKTOR) W KTÓRYM BÊDZIE ZNAJDOWA£ SIÊ NAPIS
+	Vector vc;
+	vc.Set(v, 0.07);
+
+	//POBIERMY WARTOŒÆ K¥TA
+	double alpha = cfg->Get_Alpha();
+
+	//RYSUJEMY LINIÊ
+	line2d(DC, transformationM, v, 0.06, v, -0.06, w, h);
+
+	//RYSUJEMY WARTOŒÆ
+	DC->DrawRotatedText(wxString::Format(wxT("%.2f"), v), (transformationM * vc).GetX(), (transformationM * vc).GetY(), alpha);
+	//DWA MIEJSCA PO PRZECINKU LICZBY ZMIENNOPRZECINKOWEJ
+}
+
+//FUNKCJA RYSUJ¥CA WARTOŒCI NA OSI Y
+void ChartClass::drawMarkY(wxDC* DC, Matrix transformationM, double v, int w, int h) {
+	
+	//PUNKT (WEKTOR) W KTÓRYM BÊDZIE ZNAJDOWA£ SIÊ NAPIS
+	Vector vc;
+	vc.Set(0.04, -v);
+
+	//POBIERMY WARTOŒÆ K¥TA
+	double alpha = cfg->Get_Alpha();
+
+	//RYSUJEMY LINIÊ
+	line2d(DC, transformationM, 0.04, -v, -0.04, -v, w, h);
+	
+	//RYSUJEMY WARTOŒÆ
+	DC->DrawRotatedText(wxString::Format(wxT("%.2f"), v), (transformationM * vc).GetX(), (transformationM * vc).GetY(), alpha);
+}
+
+//FUNKCJA RYSUJ¥CA DAN¥ FUNKCJÊ
+void ChartClass::drawFunction(wxDC* DC, Matrix transformationM, int w, int h, double step) {
+	
+	//ZMIENIAMY KOLOR NA CZERWONY
+	DC->SetPen(wxPen(wxColour(*wxGREEN)));
+
+	//RYSUJEMY FUNKCJE "STEP BY STEP"
+	for (double x = cfg->Get_x_start(); x <= cfg->Get_x_stop(); x += step) {
+		line2d(DC, transformationM, x, -GetFunctionValue(x), x + step, -GetFunctionValue(x + step), w, h);
+	}
+}
+
+//FUNKCJA USTALAJ¥CA KROK
+void ChartClass::setStep(double& step) {
+
+	//POBIERAMY WARTOŒCI ZA POMOC¥ FUNKCJI GET_[ELEMENT]
+	double x0 = cfg->Get_x_start();
+	double x1 = cfg->Get_x_stop();
+
+	step = (x1 - x0) / static_cast<double>(x_step);
+}
---
+++ Lab4 - WxWidgets2D/ChartClass.h
@@ -0,0 +1,48 @@
+#ifndef __ChartClass__
+#define __ChartClass__
+
+#include <memory>
+#include "ConfigClass.h"
+class Matrix;
+
+class ChartClass
+{
+
+private:
+    std::shared_ptr<ConfigClass> cfg;
+    int x_step;         // liczba odcinkow z jakich bedzie sie skladal wykres
+    double x_min, x_max; // zakres zmiennej x
+    double y_min, y_max; // zakres wartosci przyjmowanych przez funkcje
+
+    //FUNKCJE PODSTAWOWE//////////////////////////////////
+    double GetFunctionValue(double x); // zwraca wartosci rysowanej funkcji
+
+    //FUNKCJE NA MACIERZACH///////////////////////////////
+    Matrix Set_Translate(double Tx, double Ty);
+    Matrix Set_Rotation(double alpha);
+    Matrix Set_Scale(int w, int h);
+
+    //FUNKCJE RYSUJ¥CE/OBS£UGUJ¥CE///////////////////////
+    void line2d(wxDC* DC, Matrix t, double x1, double y1, double x2, double y2, int w, int h);
+    void setTransformM(Matrix& transformationM, double x0, double y0, int w, int h);
+    void setCenter(double& x0, double& y0, int w, int h);
+    void drawAxes(wxDC* DC, Matrix transformationM, int w, int h);
+    void drawArrows(wxDC* DC, Matrix transformationM, int w, int h);
+    void drawMarkX(wxDC* DC, Matrix transformationM, double v, int w, int h);
+    void drawMarkY(wxDC* DC, Matrix transformationM, double v, int w, int h);
+    void drawFunction(wxDC* DC, Matrix transformationM, int w, int h, double step);
+    void setStep(double& step);
+
+public:
+    
+    //FUNKCJE PODSTAWOWE//////////////////////////////////
+    ChartClass(std::shared_ptr<ConfigClass> c);
+    void Set_Range();   // ustala wartosci zmiennych x_min,y_min,x_max,y_max
+    double Get_Y_min(); // zwraca y_min
+    double Get_Y_max(); // zwraca y_max
+
+    //FUNKCJE RYSUJ¥CE////////////////////////////////////
+    void Draw(wxDC* DC, int w, int h);  // rysuje wykres
+};
+
+#endif
